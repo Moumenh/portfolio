bcf4402e5f9647108d8d1f160f17369fd2f9591d
Moumenh <mohanbali94@gmail.com>
2020-12-28 22:16:33 +0200

    adding Project

--- Backend/projects/models.py
+++ Backend/projects/models.py
@@ -5,8 +5,10 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=250)
-    image = models.CharField(max_length=250)
-    link = models.CharField(max_length=250)
+    desc = models.CharField(max_length=1000)
+    image = models.CharField(max_length=1000)
+    link = models.CharField(max_length=1000)
+    github = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.title
@@ -14,6 +16,7 @@ class Project(models.Model):
 
 class Technologies(models.Model):
     name = models.CharField(max_length=250)
+    icon = models.CharField(max_length=1000)
     ProjectId = models.ForeignKey(Project, on_delete=models.CASCADE,related_name='tech')
 
     def __str__(self):
@@ -23,7 +26,7 @@ class Technologies(models.Model):
 class Blogs(models.Model):
     title = models.CharField(max_length=250)
     content = models.CharField(max_length=250)
-    image = models.CharField(max_length=250)
+    image = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.title
