c56dfca5502c72defc7b1fac41f6f0023ced7a33
Moumenh <mohanbali94@gmail.com>
2020-12-28 23:09:20 +0200

    project card & about me responsive

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Components/Header/Header'
 import Intro from './Components/Intro/Intro'
 import About from './Components/About/About'
+import Projects from './Components/Projects/Projects'
 
 import './App.css';
 
@@ -11,6 +12,7 @@ const App = () => {
       <Header/>
       {/* <Intro/> */}
       <About/>
+      <Projects/>
     </div>
   );
 }
---
+++ client/src/Components/Projects/ProjectCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const ProjectCard = () => {
+    return (
+        <div className='project'>
+            <div className='project__img'>
+
+            </div>
+
+            <div className='project__content'>
+                <div>
+                    <h1> Title </h1>
+                    <div className='project__tech'></div>
+                    <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur modi aperiam animi assumenda unde aspernatur debitis eius? Consequatur in a adipisci laborum suscipit nemo ex veritatis non, quas debitis provident.</h4>
+                </div>
+                <div className='project__btn'>
+                    <a className='btn' style={{ color: '#4989ce', borderColor: '#4989ce' }}> See Live </a>
+                    <a className='btn' style={{ color: '#4989ce', border:'none', marginLeft:'30px' }}> Source Code </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default ProjectCard
