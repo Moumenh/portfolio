47730917cf4fc6f0a436aab64e4723777d6c6fe4
Moumenh <mohanbali94@gmail.com>
2021-01-01 12:19:24 +0200

    nginx for front

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'projects',
-    'corsheaders'
+    'corsheaders',
+    'import_export',
 ]
 
 MIDDLEWARE = [
--- Backend/projects/admin.py
+++ Backend/projects/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
-from projects.models import Blogs,Project,Technologies
+from projects.models import Blogs, Project, Technologies
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
-admin.site.register(Project)
-admin.site.register(Blogs)
-admin.site.register(Technologies)
+# admin.site.register(Project)
+# admin.site.register(Blogs)
+# admin.site.register(Technologies)
+
+
+@admin.register(Project)
+class ProjectAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(Technologies)
+class TechnologieAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(Blogs)
+class BlogAdmin(ImportExportModelAdmin):
+    pass
