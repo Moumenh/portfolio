cab661addfbeb6b225f8659fa9ce8e9c4187d263
Moumenh <mohanbali94@gmail.com>
2021-01-01 19:42:44 +0200

    nginx serving back and front

--- client/src/Components/Projects/Projects.jsx
+++ client/src/Components/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import ProjectCard from './ProjectCard'
 
 import './styles.css'
@@ -8,25 +8,25 @@ const Projects = () => {
 
     useEffect(() => {
         getProjects()
-    },[])
+    }, [])
 
     const getProjects = () => {
         const requestOptions = {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' }
         };
-        fetch('http://127.0.0.1:8000/projects/', requestOptions)
+        fetch('/projects/', requestOptions)
             .then(response => response.json())
             .then(data => setProjects(data))
     }
 
-    return(
+    return (
         <div className='projects'>
-            <h1 style={{fontFamily:'Montserrat,sans-serif', color:'#212529',marginBottom:'150px', fontWeight:'100'}}> Projects </h1>
+            <h1 style={{ fontFamily: 'Montserrat,sans-serif', color: '#212529', marginBottom: '150px', fontWeight: '100' }}> Projects </h1>
             {
-                projects.length && projects.map((project, i) => <ProjectCard project={project} key={i}/>)
+                projects.length && projects.map((project, i) => <ProjectCard project={project} key={i} />)
             }
-            
+
         </div>
     )
 }
