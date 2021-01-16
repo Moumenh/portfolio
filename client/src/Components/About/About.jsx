719e7cf088bc3732286d78eb139240818b1ac371
Moumenh <mohanbali94@gmail.com>
2021-01-16 22:43:56 +0200

    fade animation

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade';
+
 import './styles.css'
+
 
 const About = () => {
     return (
         <div id='about'>
             <h1 style={{ marginBottom: '60px' }}> About Me </h1>
             <div className='about__cointainer'>
-                <img className='about__pic' src='https://m0moooz-portfolio.netlify.app/assets/profile.0724d76bcb44cbb1cf44b3f18c933be9.jpg' alt='pic' />
-                <div className='about__content'>
-                    <h4 className='about__disc'> Self-Thought programmer who is a technology enthusiastic, and figuring out how everything works. </h4>
-                    <a href='https://docdro.id/LR74qe1' target="_blank" className='btn'> View Resume </a>
-                </div>
+                <Fade left> <img className='about__pic' src='https://m0moooz-portfolio.netlify.app/assets/profile.0724d76bcb44cbb1cf44b3f18c933be9.jpg' alt='pic' /></Fade>
+                <Fade right>
+                    <div className='about__content'>
+                        <h4 className='about__disc'> Self-Thought programmer who is a technology enthusiastic, and figuring out how everything works. </h4>
+                        <a href='https://docdro.id/LR74qe1' target="_blank" className='btn'> View Resume </a>
+                    </div>
+                </Fade>
             </div>
 
 
--- client/src/Components/Projects/ProjectCard.jsx
+++ client/src/Components/Projects/ProjectCard.jsx
@@ -1,30 +1,36 @@
 import React from 'react'
 import Technology from './Technology'
+import Fade from 'react-reveal/Fade';
 
-const ProjectCard = ({project}) => {
+const ProjectCard = ({ project }) => {
     console.log(project)
 
     return (
         <div className='project'>
-            <div className='project__img' style={{backgroundImage:`url(${project.image})`}}>
+            <Fade left>
+                <div className='project__img' style={{ backgroundImage: `url(${project.image})` }}>
 
-            </div>
+                </div>
+            </Fade>
 
-            <div className='project__content'>
-                <div>
-                    <h1> {project.title} </h1>
-                    <div className='project__tech'>
-                    {
-                        project.tech && project.tech.map((tech, i) => <Technology tech={tech} />)
-                    }
+            <Fade right>
+                <div className='project__content'>
+                    <div>
+                        <h1> {project.title} </h1>
+                        <div className='project__tech'>
+                            {
+                                project.tech && project.tech.map((tech, i) => <Technology tech={tech} />)
+                            }
+                        </div>
+                        <h4 style={{ fontWeight: '100' }}>{project.desc}</h4>
                     </div>
-                    <h4 style={{fontWeight:'100'}}>{project.desc}</h4>
+                    <div className='project__btn'>
+                        <a className='btn' target="_blank" href={project.link} style={{ color: '#074f94', borderColor: '#074f94' }}> See Live </a>
+                        <a className='btn' target="_blank" href={project.git} style={{ color: '#074f94', border: 'none', marginLeft: '30px' }}> Source Code </a>
+                    </div>
                 </div>
-                <div className='project__btn'>
-                    <a className='btn' target="_blank" href={project.link} style={{ color: '#074f94', borderColor: '#074f94' }}> See Live </a>
-                    <a className='btn' target="_blank" href={project.git} style={{ color: '#074f94', border:'none', marginLeft:'30px' }}> Source Code </a>
-                </div>
-            </div>
+            </Fade>
+
         </div>
     )
 }
