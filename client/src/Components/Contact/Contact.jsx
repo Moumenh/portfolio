d6ac5e42f3fd107767a6e49ab5cc3da199651e6d
Moumenh <mohanbali94@gmail.com>
2021-01-16 22:49:04 +0200

    completing animations

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade';
+
 import './styles.css'
 
 const Contact = () => {
     return (
         <div id='contact'>
-            <h1 className='contact__title'> Contact </h1>
-            <p className='contact__email'> mohanbali94@gmail.com </p>
-            <a target="_blank" href="mailto:mohanbali94@gmail.com" className='btn'> Call To Action </a>
+            <Fade>
+                <h1 className='contact__title'> Contact </h1>
+            </Fade>
+            <Fade bottom>
+                <p className='contact__email'> mohanbali94@gmail.com </p>
+                <a target="_blank" href="mailto:mohanbali94@gmail.com" className='btn'> Call To Action </a>
+            </Fade>
         </div>
     )
 }
